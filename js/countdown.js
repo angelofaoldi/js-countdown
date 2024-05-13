c91c6e98465d9a416649eb5062842c3c0246554e
angelofaoldi <faoldiangelo@gmail.com>
2024-05-13 18:31:39 +0200

    js: countdown first approach

--- js/countdown.js
+++ js/countdown.js
@@ -1,2 +1,21 @@
 'use strict';
 
+// CREO IL TEMPO DEL COUNTDOWN
+let tempo = 10;
+
+// CREO IL COUNTDOWN
+// IMPOSTO UN INTERVALLO CON UNA FUNZIONE CON STRUTTURA IF ELSE
+
+let countdown = setInterval(function() {
+    if (tempo <= 0) {
+        clearInterval(countdown);
+        console.log(alert("Buon Anno!"));
+    } else {
+        alert("Il countdown inizierà tra " + tempo + " secondi");
+    }
+    countdown -= 1;
+}, 1000);
+
+console.log(tempo);
+console.log(countdown);
+
